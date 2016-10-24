3c56dbddbc4b1f391b83603af23216a4967a0780
Vincent Andersson <vincent.andersson@gmail.com>
2016-10-24 19:48:29 +0200

    refactor

---
+++ src/main/java/no/agricult/agrihub/infrastructure/web/JAXRSConfiguration.java
@@ -0,0 +1,32 @@
+package no.agricult.agrihub.infrastructure.web;
+
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.templateresolver.ServletContextTemplateResolver;
+
+import javax.enterprise.inject.Produces;
+import javax.servlet.ServletContext;
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+/**
+ * @Author: vda
+ */
+@ApplicationPath("/app")
+public class JAXRSConfiguration extends Application {
+
+    @javax.ws.rs.core.Context
+    ServletContext context;
+
+    @Produces
+    public TemplateEngine templateEngine() {
+        TemplateEngine templateEngine = new TemplateEngine();
+        ServletContextTemplateResolver templateResolver = new ServletContextTemplateResolver(context);
+        templateResolver.setPrefix("/WEB-INF/templates/");
+        templateResolver.setSuffix(".html");
+        // Set template cache TTL to 1 hour. If not set, entries would live in cache until expelled by LRU
+        templateResolver.setCacheTTLMs(Long.valueOf(3600000L));
+        templateEngine.setTemplateResolver(templateResolver);
+        return templateEngine;
+    }
+
+}
